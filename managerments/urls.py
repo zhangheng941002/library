0df477ebb189a32bcbe193c05286e8170669ac71
zhangheng <zhangheng9394@163.com>
2019-03-05 09:08:34 +0800

    设置黑名单

--- managerments/urls.py
+++ managerments/urls.py
@@ -1,13 +1,9 @@
-# from django.conf.urls import url
-# from rest_framework import routers
-# from .views import *
-# from .api_views import *
-# # from rest import views as rest_views
-# router = routers.DefaultRouter(trailing_slash=False)
-#
-# router.register(r'contract_manage', ContractManage, base_name='contract_manage')
-# router.register(r'contract_payment', ContractPaymentInfo, base_name='contract_payment')
-#
-# urlpatterns = router.urls
-# urlpatterns.append(url(r'contract_upload', contract_upload))
-# urlpatterns.append(url(r'contract_download', contract_download))
+from django.conf.urls import url
+from rest_framework import routers
+from .views import *
+# from rest import views as rest_views
+router = routers.DefaultRouter(trailing_slash=False)
+
+urlpatterns = router.urls
+urlpatterns.append(url(r'contract_upload', contract_upload))
+urlpatterns.append(url(r'contract_download', contract_download))
--- managerments/views.py
+++ managerments/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.db.transaction import atomic
+from seat.models import BlankLogs
+from .serializers import *
+import json
+import datetime
+
+from utils.page_kit import get_page_limit
+
+
+# 设置黑名单
+@api_view(["POST"])
+def set_blank(request):
+    """
+
+    """
+    data = request.data
+    user_id = data.get("user_id")
+
+    data_set = {
+        "user_id": user_id,
+        "status": 1,
+    }
+    seat_date = BlankLogs.objects.filter(**data_set)
+    if seat_date:
+        return Response({"statusCode": 1, "msg": "拉黑成功成功！", })
+    else:
+        return Response({"statusCode": 0, "msg": "拉黑失败！", })
