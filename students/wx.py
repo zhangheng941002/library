424be746fb37c801aefc5b30ea3bf250ea7603a8
zhangheng <zhangheng9394@163.com>
2019-05-08 00:03:25 +0800

    通过微信发送验证码，itchat

--- students/wx.py
+++ students/wx.py
@@ -1,6 +1,8 @@
-from django.http import JsonResponse
 import requests, json
 import random
+import itchat
+
+itchat.auto_login(hotReload=True)
 
 
 def send_message_to_me():
@@ -17,18 +19,26 @@ def send_message_to_me():
         "text": "开始学习的验证码",
         "desp": "您的验证码是 {}".format(rand_str),
     }
-    resp = requests.post(url=URL_ALL_SEND, data=data)
-    resp = json.loads(resp.content)
-    # print(resp)
-    if resp.get("code") == 0:
-        print(resp.get("data"))
-        return {"status": 1, "wxyzm": rand_str}
-    else:
-        return {"status": 0, "msg": "发送验证码失败"}
+    # resp = requests.post(url=URL_ALL_SEND, data=data)
+    # resp = json.loads(resp.content)
+    # # print(resp)
+    # if resp.get("code") == 0:
+    #     print(resp.get("data"))
+    #     return {"status": 1, "wxyzm": rand_str}
+    # else:
+    #     return {"status": 0, "msg": "发送验证码失败"}
 
     # response = JsonResponse({'st': rand_str})
     # response.set_cookie('wxyzm', rand_str)
     # return response
+
+    users = itchat.search_friends(name='张衡')  # 使用备注名来查找实际用户名
+    # 获取好友全部信息,返回一个列表,列表内是一个字典
+    print(users)
+    # 获取`UserName`,用于发送消息
+    userName = users[0]['UserName']
+    itchat.send("学习的验证码为：{}".format(rand_str), toUserName=userName)
+    return {"status": 1, "wxyzm": rand_str}
 
 
 if __name__ == '__main__':
